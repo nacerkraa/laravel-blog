595902146f5bf9ae119d48972bbbd6d9ee317a4a
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-04-20 01:43:44 +0000

    start new project laravel-8

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,6 @@
                             <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-gray-500 underline">Register</a>
                         @endif
                     @endauth
-
                 </div>
             @endif
 
