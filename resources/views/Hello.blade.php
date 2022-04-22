80d0582b138a49361d593ce9873efa3a3339dfba
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-04-22 21:53:04 +0000

    add if else to the home template

--- resources/views/Hello.blade.php
+++ resources/views/Hello.blade.php
@@ -1,2 +1,8 @@
-<x-header data="Component Header"/>
-<h3>Hello {{$name}}</h3>
+<x-header data="Hello Component Header"/>
+
+
+@if($name == 'nacer') 
+    <h3>Hello {{$name}} you are the admin</h3>   
+@else
+      <td> Guest </td>   
+@endif
