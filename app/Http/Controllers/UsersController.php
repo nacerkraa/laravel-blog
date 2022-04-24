c565e1c485a3c39f2d328e359f410216c7705c21
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-04-24 01:48:28 +0000

    add validation to the form

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,10 @@ use Illuminate\Http\Request;
 class UsersController extends Controller
 {
     public function getData(Request $req){
+        $req -> validate([
+            'user' =>'required | max:10',
+            'password' =>'required | min:5'
+        ]);
         $data = $req -> input();
         return "Hello " . $data['user'] . ", your password is: ". $data['password'];
     }
--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -4,7 +4,18 @@
 <h2>Login User</h2>
 <form action="users" method="POST">
     @csrf
+    
+        @error('user')
+        <span style="color: red">{{$message}}</span><br><br>
+        @enderror
+    
+    
     <input type="text" name="user" placeholder="Enter your user name"><br><br>
+    
+        @error('password')
+            <span style="color: red">{{$message}}</span><br><br>
+        @enderror
+    
     <input type="password" name="password" placeholder="Enter your password"><br><br>
     <button type="submit">Login</button>
 </form>
