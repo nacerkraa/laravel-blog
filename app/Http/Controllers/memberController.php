7466830c06fb0d490c9e522e3102cce1644d3e44
nacer kraa <nacer.kraa@univ-constantine2.dz>
2022-04-26 00:48:03 +0000

    save data in database

--- app/Http/Controllers/memberController.php
+++ app/Http/Controllers/memberController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Member;
 
 class memberController extends Controller
 {
     function addData(Request $req)
     {
+        $member = new Member;
+        $member -> name = $req -> name;
+        $member -> email = $req -> email;
+        $member -> adress = $req -> adress;
 
+        $member -> save();
     }
 }
--- app/Models/Member.php
+++ app/Models/Member.php
@@ -8,4 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class Member extends Model
 {
     use HasFactory;
+    public $timestamps = false;
 }
