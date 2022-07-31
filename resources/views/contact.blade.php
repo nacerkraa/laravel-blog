328a1d68d22935d2f9d6d718bf4ddf4aea4e5547
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-31 22:30:38 +0100

    Add layout to the all the pages

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home | Contact</title>
-</head>
-<body>
+@extends('layouts.app')
+
+@section('content')
     <h2>This contact page</h2>
-   
-</body>
-</html>
+@endsection
+    
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
-</head>
-<body>
-    <h2>This is home page</h2>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <h2>This is Home page</h2>
+@endsection
