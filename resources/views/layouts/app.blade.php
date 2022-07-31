f2d7fcc47e2ded586771d76dca4ff2b0e7103e91
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-31 22:42:31 +0100

    Add navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Home</title>
+    <title>My Super Blog</title>
 </head>
 <body>
+       @include('partials.navbar')
        @yield('content')
 </body>
 </html>
