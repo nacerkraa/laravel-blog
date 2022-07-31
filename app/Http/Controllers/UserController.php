29f6f0205d980c5d8d84feb03336e4c6153d130a
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-31 18:16:05 +0100

    Send title from controller to the view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ class UserController extends Controller
 {
     public function index()
     {
-        return view('hello');
+        $title = "The title of this blog";
+        return view('hello', compact('title'));
     }
 }
