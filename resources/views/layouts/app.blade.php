cb03ed17510a047b9be036f9bd7e702b006e927e
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-31 23:57:19 +0100

    Add stayling

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,10 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>My Super Blog</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    
 </head>
 <body>
        @include('partials.navbar')
        @yield('content')
        @include('partials.footer')
+       
+       <script src="{{asset('js/app.js')}}"></script>
 </body>
 </html>
