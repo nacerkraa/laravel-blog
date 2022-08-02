0e6fe87b6792135a0480427755cbfe3929e84a36
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-08-02 12:09:52 +0100

    Create Post moder and migration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('Home');
-});
-
-Route::get('/post', []);
+Route::get('/', [PostController::class,'index']) -> name("home");
+Route::get('/post', [PostController::class,'show']) -> name("post");
