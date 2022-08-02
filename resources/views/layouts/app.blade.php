a953a9e76d0f090bc0a623cc97a8444442687535
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-08-02 20:34:45 +0100

    add and js style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,9 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+    <link rel="stylesheet" href={{asset('css/app.css')}}>
+    
 </head>
 <body>
     @include('components.navbar')
     @yield('content')
+    <script src={{asset('js/app.js')}}></script>
+    
 </body>
 </html>
