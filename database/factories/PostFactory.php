40ad2fc7f22293cbb7ab38e5e0cace0d900004bb
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-08-02 13:22:27 +0100

    Import Post class

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class PostFactory extends Factory
@@ -11,6 +12,8 @@ class PostFactory extends Factory
      *
      * @return array
      */
+    protected $model = Post::class;
+    
     public function definition()
     {
         return [
