3b526feedc54551ef68075b7904ab479ac32ba7d
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-08-02 12:10:58 +0100

    Update to post

--- database/migrations/2022_08_02_111041_create_posts_table.php
+++ database/migrations/2022_08_02_111041_create_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostControllersTable extends Migration
+class CreatePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePostControllersTable extends Migration
      */
     public function up()
     {
-        Schema::create('post_controllers', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -26,6 +26,6 @@ class CreatePostControllersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('post_controllers');
+        Schema::dropIfExists('posts');
     }
 }
