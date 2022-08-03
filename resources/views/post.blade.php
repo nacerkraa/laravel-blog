dcdcc915e0342ef4cb4230e3e68abc039d7414ed
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-08-03 22:19:58 +0100

    Apply if and else

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.app')
 @section('content')
     <h2>List Of Posts</h2>
-
-    @foreach ($posts as $post)
-    <ul>
-        <li><a href="#">{{$post -> title}}</a></li>
-    </ul>
-        
-    @endforeach    
+    @if ($posts-> count() > 0)
+        @foreach ($posts as $post)
+            <ul>
+                <li><a href="#">{{$post -> title}}</a></li>
+            </ul>
+        @endforeach        
+    @else
+        <span>There is no post</span>
+    @endif
+    
 @endsection
 
