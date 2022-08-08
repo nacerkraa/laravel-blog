67de9c76905234d26a254368fe578950e842b070
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-08-08 23:50:15 +0100

    add crf to the form

--- resources/views/createPost.blade.php
+++ resources/views/createPost.blade.php
@@ -3,6 +3,7 @@
 @section('content')
     <h2>This is create post page</h2>
     <form action="{{route('post.store')}}" method="post">
+        @csrf
         <input type="text" name="title" id="">
         <textarea name="content" id="" cols="30" rows="10"></textarea>
         <button type="submit">Create</button>
