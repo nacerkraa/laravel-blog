bc02b6f8cb3b66b9f94bf1331e298d9d23b9b0ca
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-08-12 02:11:56 +0100

    Add a new row in migration comment

--- database/migrations/2022_08_11_234933_create_comments_table.php
+++ database/migrations/2022_08_11_234933_create_comments_table.php
@@ -15,6 +15,7 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table -> mediumText();
             $table->timestamps();
         });
     }
