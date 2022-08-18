4114910d927376d3fc59bc3bb6e99afea89e1426
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-08-18 19:19:22 +0100

    Update list of comments styling

--- resources/views/comment-list.blade.php
+++ resources/views/comment-list.blade.php
@@ -1,13 +1,15 @@
 @extends('layouts.app')
 @section('content')
-<h2>List Of Comments</h2>
-@if ($comments-> count() > 0)
-    @foreach ($comments as $comment)
-        <ul>
-            <li><a>{{$comment -> content}}</a></li>
-        </ul>
-    @endforeach 
-@else
-    <span>There is no comments</span>
-@endif
+    <div class="container">
+        <h2>List Of Comments</h2>
+        @if ($comments-> count() > 0)
+        @foreach ($comments as $comment)
+            <ul>
+                    <li><a>{{$comment -> content}}</a></li>
+                </ul>
+                @endforeach 
+        @else
+            <span>There is no comments</span>
+        @endif
+    </div>
 @endsection
